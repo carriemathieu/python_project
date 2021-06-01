6ea6cc434530d0b79ac2d7571cb8d90d23630755
Carrie Mathieu <carrieburks@gmail.com>
2021-05-31 22:46:31 -0400

    fixed venv

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# creates instance of flask & assigns to app var
+app = Flask(__name__)
+
+# function that returns 'hello world'
+@app.route('/')
+def hello_world():
+    return "Hello world"
+
+# if app.py is executed directly, run app (flask server)
+if __name__ == '__main__':
+    app.run(debug=True)
